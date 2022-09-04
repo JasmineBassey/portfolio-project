eb7ef95d179801d8be77e7cf0575d4f9b05f6661
Jasmine Bassey <87097983+JasmineBassey@users.noreply.github.com>
2022-09-04 20:44:42 +0200

    Delete Portfolio directory

--- Portfolio/Resources/js/index.js
+++
@@ -1,9 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('nav__list')[0]
-
-toggleButton.addEventListener('click', (e) => { 
-    e.preventDefault();
-    navbarLinks.classList.toggle('active');
-})
-
-
